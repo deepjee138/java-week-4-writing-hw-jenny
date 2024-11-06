1fe4f82acede038f560c3358976cf7059435df5e
deepjee138 <deepjenny11@gmail.com>
2024-11-06 21:48:04 +0000

    done

---
+++ src/homework_week_8/programme_23_method_overriding/Vehicle.java
@@ -0,0 +1,14 @@
+package homework_week_8.programme_23_method_overriding;
+
+/**
+ *  * Creating a child class
+ */
+
+public class Vehicle {
+    //defining a method
+    public void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
+
